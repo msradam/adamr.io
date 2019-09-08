035dc3e2be227b772429ebba40a4c18acf4ec7b9
Adam Rahman <19861960+msradam@users.noreply.github.com>
2019-09-08 17:35:01 -0400

    Fixed landing page.

--- data/projects.js
+++ data/projects.js
@@ -1,4 +1,14 @@
 const projects = [
+   {
+    icon: '🎭',
+    title: 'Wesleyan Theater DesignerBase',
+    path: 'https://wes-theater-designerbase.herokuapp.com',
+    source: 'https://github.com/msradam/wes-theater-designerbase',
+    description:
+      'A designer database for the Wesleyan student theater community',
+    img: '',
+    tech: 'Node.js, Express, Google Sheets, Typeform, React'
+  },
   {
     icon: '⚖️',
     title: 'Carceral Contagion',
@@ -11,41 +21,33 @@ const projects = [
     tech: 'Python, Mesa, D3, NetworkX'
   },
   {
-    icon: '🎭',
-    title: '2nd Stage Designer Database',
-    path: 'https://second-stage-designerbase.herokuapp.com',
-    source: 'https://github.com/msradam/2nd-stage-designerbase',
-    description:
-      'A designer database for the Wesleyan 2nd Stage Theater Company.',
-    img: '',
-    tech: 'Node.js, Express, Google Sheets, Typeform, React'
-  },
-  {
-    icon: '💠',
-    title: 'RESP',
-    source: 'https://github.com/msradam/resp',
-    description:
-      'A psychological first-aid app built for first-responders in natural disasters.',
-    img: '',
-    tech: 'Django REST Framework, React'
-  },
-  {
     icon: '🌐',
-    title: 'Magicbox Geospatial Routing',
-    source: 'https://github.com/msradam/magicbox-site-routing',
+    title: 'Magicbox Routing API',
+    path: 'https://magicbox-routing.herokuapp.com',
+    source: 'https://github.com/msradam/magicbox-routing-api',
     description:
       'Utilities for UNICEF Magicbox developers to perform geospatial calculations on large datasets.',
     img: '',
     tech: 'Django REST Framework, Scikit-learn, iGraph, ReDoc, Docker'
   },
   {
-    icon: '🌺',
-    title: 'Wellspring',
-    source: 'https://github.com/avarenee/wellspring',
+    icon: '💠',
+    title: 'RESP',
+    path: 'https://resp-angelhack.herokuapp.com',
+    source: 'https://github.com/msradam/resp',
     description:
-      'A cognitive behaviorial therapy app that allows one to journal their thoughts.',
+      'A psychological first-aid app built for first-responders in natural disasters.',
     img: '',
-    tech: 'Django, React, Typeform'
-  }
+    tech: 'Django REST Framework, React'
+  },  
+  // {
+  //   icon: '🌺',
+  //   title: 'Wellspring',
+  //   source: 'https://github.com/avarenee/wellspring',
+  //   description:
+  //     'A cognitive behaviorial therapy app that allows one to journal their thoughts.',
+  //   img: '',
+  //   tech: 'Django, React, Typeform'
+  // }
 ];
 export default projects;
